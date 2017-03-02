a4ea2c209c2a306c0e2db04ca425bd3a797fa31a
mirman-mtaggart <mtaggart@mirman.org>
2017-03-02 13:24:52 -0800

    Finish README

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,11 @@ module.exports = {
   entry: "./src",
   output: {
     path: path.join(__dirname, "public/assets/js"),
-    publicPath: "/assets/js/",
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: "/assets/js"
+  },
+  devServer: {
+    contentBase: "public"
   },
   module: {
     preLoaders: [{
@@ -17,14 +20,10 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: "babel-loader"
+    }, {
+      test: /\.json$/,
+      loaders: ["json-loader"]
     }]
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    })
-  ],
   devtool: "source-map"
 }
